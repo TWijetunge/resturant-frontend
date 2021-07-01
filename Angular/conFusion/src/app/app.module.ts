86b1668b3b99a9cf0105a63674f9f81f5abb6e4a
Tharindu Wijetunge <tharinduwijetunge@gmail.com>
2021-07-02 00:22:11 +0530

    components finished

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -4,20 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; //imported 
 import {MatToolbarModule} from '@angular/material/toolbar';                   //imported
 import {FlexLayoutModule} from '@angular/flex-layout';                        //imported
+import {MatListModule} from '@angular/material/list';                         //imported
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule}   from '@angular/material/card';
+import {MatButtonModule}   from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
-import 'hammerjs';                                                           //imported
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';                                                           //imported
 
 @NgModule({          //this is a decorator which is also a function and it enables us to modify javascript ; this contains some metadata as follows
   declarations: [
-    AppComponent
+    AppComponent,    //all components are declared in the module
+    MenuComponent, DishdetailComponent    // recently created new "menu" component is included in this decorator.................................
   ],
-  imports: [
+  imports: [         // all imports are declared here
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
