e5c1a3870fb048d25828f2aa6a38a94f44e74864
Tharindu Wijetunge <tharinduwijetunge@gmail.com>
2021-08-27 17:24:05 +0530

    databindingover

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -5,20 +5,22 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; //
 import {MatToolbarModule} from '@angular/material/toolbar';                   //imported
 import {FlexLayoutModule} from '@angular/flex-layout';                        //imported
 import {MatListModule} from '@angular/material/list';                         //imported
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule}   from '@angular/material/card';
-import {MatButtonModule}   from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';                //imported
+import {MatCardModule}   from '@angular/material/card';                       //imported
+import {MatButtonModule}   from '@angular/material/button';                   //imported
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';                                                           //imported
+import { DishdetailComponent } from './dishdetail/dishdetail.component'; 
+                                                          //imported
 
 @NgModule({          //this is a decorator which is also a function and it enables us to modify javascript ; this contains some metadata as follows
   declarations: [
     AppComponent,    //all components are declared in the module
-    MenuComponent, DishdetailComponent    // recently created new "menu" component is included in this decorator.................................
+    MenuComponent,
+    DishdetailComponent    // recently created new "menu" component is included in this decorator.................................
   ],
   imports: [         // all imports are declared here
     BrowserModule,
--- Angular/conFusion/src/app/menu/menu.component.ts
+++ Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,48 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Dish} from '../shared/dish';            //when we import a file from a folder, we use ../ in order to access the folder first.
+import {DISHES} from '../shared/dishes';
 
 
-const DISHES: Dish[] = [
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: 'assets/images/images/uthappizza.png',
-    category: 'manins',
-    featured: true,
-    label: 'hot',
-    price: '4.99',
-    description:'A unique combination of indian Uthappam'
-  },
-  {
-    id: '1',
-    name: 'Zucchipakoda',
-    image: 'assets/images/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description:'Deep fried Zucchini coated with mildly sp'
-  },
-  {id: '2',
-  name: 'Vadonut',
-  image: 'assets/images/images/vadonut.png',
-  category: 'appetizer',
-  featured: false,
-  label: 'New',
-  price: '1.99',
-  description:'A quintessential ConFusion experience'
-},
-{
-  id: '3',
-  name: 'ElaiCheese Cake',
-  image: 'assets/images/images/elaicheesecake.png',
-  category: 'dessert',
-  featured: false,
-  label: '',
-  price: '2.99',
-  description:'A delectable, semi-sweet New York Style '
-}
-]; 
 
 @Component({
   selector: 'app-menu',
@@ -53,11 +13,19 @@ export class MenuComponent implements OnInit {
 
   dishes: Dish[] = DISHES;
 
-  //selectedDish: Dish = DISHES[0];
+ selectedDish: Dish = new Dish;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSelect(dish:Dish){
+    this.selectedDish=dish;
+  }
+
+  
 
 }
