1e2cd6de46a24b7b93b13440999427355a0d4bb1
Tharindu Wijetunge <tharinduwijetunge@gmail.com>
2021-09-06 15:05:25 +0530

    spa_2

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 //import { DISHES} from '../shared/dishes';
 
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
+
+
 
 
 @Component({
@@ -11,12 +16,21 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()                           // @Input()  gives a child component a way to communicate with its parent component
+ // @Input()                           // @Input()  gives a child component a way to communicate with its parent component
+                                      //@Input is removed,as router parameters substituted
   dish: Dish = new Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService, 
+              private location: Location,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let  id =this.route.snapshot.params['id'];
+    this.dish= this.dishService.getDish(id);
+  }
+
+  goBack(): void{
+   this.location.back();
   }
 
 }
